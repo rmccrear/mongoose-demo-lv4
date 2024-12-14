4f4235a0225a8666020121a050dd4c153f972008
Robert McCreary Mannino <fooyinfighter@gmail.com>
2024-12-14 08:02:06 -0800

    revert to express-mongo starter

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // index.js
 const express = require('express');
 const mongoose = require('mongoose');
-const Song = require("./models/songs"); 
 const Cat = require("./models/cats"); 
-const Book = require("./models/books");
 
 
 require('dotenv').config();
@@ -50,39 +48,6 @@ app.post('/cats', async (req, res) => {
   // return the cat object back 
   res.json(savedCat);
 });
-
-app.post("/books", async (req, res) => {
-  const myBook = new Book( {
-      title: "Recursion",
-      artist: "Blake Crouch",
-      year: 2019
-  })
-  // save to the database
-  const savedBook = await myBook.save();
-  console.log(savedBook)
-
-  // return the cat object back 
-  res.json(savedBook);
-});
-
-app.post('/songs', async (req, res) => {
-  const favSong = new Song({
-      title: "La Mer",
-      artist: "Claude Debussy",
-      genre: "classical",
-  });
-
-  //save to database
-  const savedSong = await favSong.save();
-  console.log(savedSong)
-
-  //return the song object back
-  res.json(savedSong);
-
-});
-
-
-
 
 
 
